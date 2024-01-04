29d993ebc6d63fe367ee1712df4505170bd0158b
Manthankk <98227419+Manthankk@users.noreply.github.com>
2024-01-04 11:49:29 +0530

    Add files via upload

---
+++ condition.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+//    int a;
+//    cin >> a;
+//    // cout << "value of n is: " << a << endl;
+//    if (a > 0) {
+//        cout << "A is positive " << a << endl;
+//    }
+//    else{
+//    	if (a<0){
+//    		cout<<'A is negative';
+//		}
+//		else{
+//			cout<<'A is 0';
+//		}
+//	}
+//    return 0;
+//      // Make sure to return an integer value from the main function
+//}
+
+int a,b;
+cout<< "Enter the value of a"<< endl;
+cin>> a;
+cout<<"Enter the value of b" << endl;
+cin>> b;
+ 
+if(a>b){
+	cout<< "A is greater" <<endl;
+}
+
+if (b>a){
+	cout<< "B is greater " << endl;
+}
+}
+
