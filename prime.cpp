0c945b80ad6bbdfe85c1f43b1d568635e90bb38e
Manthankk <98227419+Manthankk@users.noreply.github.com>
2024-01-04 12:21:38 +0530

    Add files via upload
    7
    prime for2
    prime for3
    prime for4
    prime for5
    prime for6

---
+++ prime.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int n;
+	cin >> n;
+	
+	int i=2;
+	while(i<n){
+		
+		if(n%i==0){
+			cout<<"Not Prime" <<i<<endl;
+		}
+		else{
+			cout<<"prime for"<<i<<endl;
+		}
+		i=i+1;
+	}
+}
