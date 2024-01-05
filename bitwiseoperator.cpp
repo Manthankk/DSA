a7026f9a867b211aca507d148da2955088885b37
Manthankk <98227419+Manthankk@users.noreply.github.com>
2024-01-05 16:25:21 +0530

    Add files via upload

---
+++ bitwiseoperator.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int a=6;
+	int b=4;
+	
+	cout<<"a&b"<<(a&b) << endl;
+	cout<<"a|b"<<(a|b) << endl;
+	cout<<"~a"<<(~a) << endl;
+	cout<<"a^b"<<(a^b) << endl;
+	
+	int i=7;
+	cout<< i++<<endl;
+	cout<< ++i<<endl;
+	cout<< --i<<endl;
+	cout<< i-- << endl;
+	
+}
