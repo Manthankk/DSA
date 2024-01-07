5ff9b8ad56742da1c6a257c63e12493b15563918
Manthankk <98227419+Manthankk@users.noreply.github.com>
2024-01-07 19:09:15 +0530

    Add files via upload

---
+++ switch.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	char ch='1';
+	int num=1;
+	
+	cout<<endl;
+	switch(num){
+		case 1:cout<<"first to come i school"<<endl;
+		break;
+		case '1':switch(num){
+			case 1:cout<<"Value of num is"<<cout<<endl;
+			
+			break;
+		}
+		break;
+		default:cout<<"this is default"<<endl;
+	}
+	cout<<endl;
+	return 0;
+}
+
+
+
